6ee7b372bcc087b09c8590cc72087cdfceec31c8
Nlaysi <62209572+Nlaysi@users.noreply.github.com>
2021-11-11 21:52:12 +0300

    Delete src/main/kotlin/com/itmo/microservices/demo/warehouse directory

--- src/main/kotlin/com/itmo/microservices/demo/warehouse/api/model/CatalogItemModel.kt
+++
@@ -1,7 +0,0 @@
-package com.itmo.microservices.demo.warehouse.api.model
-
-data class CatalogItemModel(
-    val title: String,
-    val description: String,
-    val price: Int
-)
--- src/main/kotlin/com/itmo/microservices/demo/warehouse/api/model/ItemQuantityChangeRequest.kt
+++
@@ -1,6 +0,0 @@
-package com.itmo.microservices.demo.warehouse.api.model
-
-import java.util.*
-
-data class ItemQuantityChangeRequest(var id: UUID, var amount: Int) {
-}
