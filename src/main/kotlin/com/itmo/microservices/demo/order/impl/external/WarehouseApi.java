ce31e078dae222675e5c1b15f180648c94ec4962
Nikita Baranov <baranovnikita18@gmail.com>
2021-11-17 12:39:22 +0300

    p09: extract request preparation method

--- src/main/kotlin/com/itmo/microservices/demo/order/impl/external/WarehouseApi.java
+++ src/main/kotlin/com/itmo/microservices/demo/order/impl/external/WarehouseApi.java
@@ -3,7 +3,6 @@ package com.itmo.microservices.demo.order.impl.external;
 import com.itmo.microservices.demo.order.api.dto.ItemQuantityRequestDTOJava;
 import com.itmo.microservices.demo.order.api.dto.OrderDto;
 import com.itmo.microservices.demo.order.api.external.IWarehouseApi;
-import com.itmo.microservices.demo.order.impl.entity.OrderEntity;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -17,11 +16,21 @@ import java.util.stream.Collectors;
 
 public class WarehouseApi implements IWarehouseApi {
 
-    private static final String API_URL = "http://77.234.215.138:30019/api/warehouse/book";
+    private static final String API_URL_PREFIX = "http://77.234.215.138:30019/api/warehouse";
 
     @Override
     public ResponseEntity<Set> book(OrderDto orderDto) {
         RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForEntity(API_URL_PREFIX + "/book", prepareRequest(orderDto), Set.class);
+    }
+
+    @Override
+    public ResponseEntity<String> unbook(OrderDto orderDto) {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.postForEntity(API_URL_PREFIX + "/unbook", prepareRequest(orderDto), String.class);
+    }
+
+    private HttpEntity<Map<String, List<ItemQuantityRequestDTOJava>>> prepareRequest(OrderDto orderDto) {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
         Map<String, List<ItemQuantityRequestDTOJava>> parameters = new HashMap<>();
@@ -33,12 +42,6 @@ public class WarehouseApi implements IWarehouseApi {
                 .collect(Collectors.toList());
         parameters.put("itemList", itemList);
 
-        HttpEntity<Map<String, List<ItemQuantityRequestDTOJava>>> request = new HttpEntity<>(parameters, headers);
-        return restTemplate.postForEntity(API_URL, request, Set.class);
-    }
-
-    @Override
-    public ResponseEntity<String> unbook(OrderDto order) {
-        return null;
+        return new HttpEntity<>(parameters, headers);
     }
 }
